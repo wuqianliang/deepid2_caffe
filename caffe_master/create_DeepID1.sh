4cb8ce369f62f8c448db16eeafe428d8cbff54ec
wuqianliang <wuqianliang@ismartearth.com>
2017-12-12 22:10:35 +0800

    update create lmdb sh

--- caffe_master/create_DeepID1.sh
+++ caffe_master/create_DeepID1.sh
@@ -2,8 +2,8 @@
 # This script converts the CASIA-maxpy-clean into lmdb format.
 set -e
 
-EXAMPLE=examples/deepid2
-DATA=data/lfw-aligned/
+EXAMPLE=examples/deepid1
+DATA=data/ms-celeb-1m/MsCelebV1-Faces-Aligned-Parsed/ 
 TOOLS=build/tools
 
 RESIZE_HEIGHT=55
@@ -11,9 +11,9 @@ RESIZE_WIDTH=47
 
 echo "creating lmdb..."
 
-rm -rf $EXAMPLE/DeepID2_train_lmdb
+rm -rf $EXAMPLE/DeepID1_train_lmdb
 
-rm -rf $EXAMPLE/DeepID2_test_lmdb
+rm -rf $EXAMPLE/DeepID1_test_lmdb
 
 
 $TOOLS/convert_imageset \
@@ -21,19 +21,19 @@ $TOOLS/convert_imageset \
     --resize_width=$RESIZE_WIDTH \
     $DATA \
     $DATA/train.txt \
-    $EXAMPLE/DeepID2_train_lmdb
+    $EXAMPLE/DeepID1_train_lmdb
 
 $TOOLS/convert_imageset \
     --resize_height=$RESIZE_HEIGHT \
     --resize_width=$RESIZE_WIDTH \
     $DATA \
     $DATA/val.txt \
-    $EXAMPLE/DeepID2_test_lmdb
+    $EXAMPLE/DeepID1_test_lmdb
 
 echo "compute image mean..."
 
-$TOOLS/compute_image_mean $EXAMPLE/DeepID2_train_lmdb \
-  $EXAMPLE/DeepID2_mean.proto
+$TOOLS/compute_image_mean $EXAMPLE/DeepID1_train_lmdb \
+  $EXAMPLE/DeepID1_mean.proto
 
 echo "done..."
 
